7c9b007a93d3aff3243910b79cbada9cf6f94f4e
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-03-09 12:56:05 -0500

    Basics complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreation = 'Click for event binding example';
+  serverName = 'Two-Way Binding Example'
+  practiceName = '';
+  empty = true;
+  serverCreated = false;
 
   constructor() { 
     setTimeout(()=> {
@@ -18,4 +23,22 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreation = 'EVENT ACTIVATED';
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdatePracticeName(event: Event) {
+    this.practiceName = (<HTMLInputElement>event.target).value;
+    this.empty = false;
+  }
+
+  resetPractice() {
+    this.practiceName = ''
+    this.empty = true;
+  }
 }
